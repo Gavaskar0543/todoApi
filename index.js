2c88bb8d6001380643f07c5b6efceb895f5d428b
Gavaskar <gavaskaraeg@gmail.com>
2023-08-11 16:00:53 +0530

    done testing on postman pending

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ const express = require('express');
 const port  = 8000;
 const app = express();
 const db = require('./config/mongoose');
-
-//app.use('/',require('./router'))
+app.use(express.urlencoded());
+app.use('/',require('./router'))
 app.listen(port,function(err){
     if(err){
         console.log(err.message);
--- router/api/v1/index.js
+++ router/api/v1/index.js
@@ -1,5 +1,9 @@
 const express = require('express');
 const router = express.Router();
+const taskController = require('../../../controller/api/v1/tasks');
 
-
+router.get('/tasks',taskController.allTask);
+router.post('/newTask',taskController.newTodo);
+router.delete('/destroy/:id',taskController.destroyTask);
+router.put('/updateTask',taskController.updateTask);
 module.export = router;
